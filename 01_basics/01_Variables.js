b076e32af96d230c5634606e1c3bb841237863b7
ChinmayJyotishi <125024269+ChinmayJyotishi@users.noreply.github.com>
2024-06-11 09:23:21 +0000

    var

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=96511
+let accountEmail="chinmay@google.com"
+var accountPassword="85476"
+accountAddress='Rairangpur'
+let accs;
+
+// accountId=45781 you cant assign a new value to a constant
+
+accountEmail='cj@gmail.com'
+accountPassword="11245"
+accountAddress='Delhi'
+
+
+
+// console.log(accountId);
+console.table([accountEmail,accountPassword,accountAddress,accs])
+
