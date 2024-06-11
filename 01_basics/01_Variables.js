b1b99da13a1f6369f1380c7c79911b1b82a1f79b
ChinmayJyotishi <125024269+ChinmayJyotishi@users.noreply.github.com>
2024-06-11 10:15:54 +0000

    variables

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -2,7 +2,7 @@ const accountId=96511
 let accountEmail="chinmay@google.com"
 var accountPassword="85476"
 accountAddress='Rairangpur'
-let accs;
+let accs; // this will be treated as undefined 
 
 // accountId=45781 you cant assign a new value to a constant
 
@@ -10,8 +10,20 @@ accountEmail='cj@gmail.com'
 accountPassword="11245"
 accountAddress='Delhi'
 
-
+// dont use var because of the issues in block scope and functional scope
 
 // console.log(accountId);
-console.table([accountEmail,accountPassword,accountAddress,accs])
+// console.table([accountEmail,accountPassword,accountAddress,accs])
 
+
+// var can be redeclared but let cant be redecalared 
+
+var a =10
+var a=55
+console.log(a);
+
+// let b=65 
+// let b=98 // you cant redeclare 
+// console.log(b);
+
+window.alert("heelo")
