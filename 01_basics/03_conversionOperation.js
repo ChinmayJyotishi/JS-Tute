535c41541a7c77850dd5832f76de36fbdac1a559
ChinmayJyotishi <125024269+ChinmayJyotishi@users.noreply.github.com>
2024-07-02 07:52:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+//   let score =33
+//   console.log(score);
+//   console.log(typeof score);
+
+  let score1= "33"
+//   console.log(score1);
+//   console.log(typeof score1);
+
+//   now convert the string in number
+// let valueInNumber= Number(score1)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score2 ="33ABC"
+// let valueInNumber= Number(score2)
+// console.log(valueInNumber); // value will come in NaN means not a number, NaN is also a type in JS
+// console.log(typeof valueInNumber);
+
+// let score3 =null
+// let valueInNumber= Number(score3)
+// console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
+
+// let score4 =undefined
+// let valueInNumber= Number(score4)
+// console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
+
+// let score5 ='chinmay'
+// let valueInNumber= Number(score5)
+// console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
+
+// Boolean Conversion
+
+// let isLoggedIn=1
+// console.log(`Before conversion ${isLoggedIn}`); 
+// console.log(`Before conversion type ${typeof isLoggedIn}`);
+// let checkBoolean =Boolean(isLoggedIn)
+// console.log(`After conversion ${checkBoolean}`); 
+// console.log(`After conversion type ${typeof checkBoolean}`); 
+
+// let isLoggedIn=0
+// console.log(`Before conversion ${isLoggedIn}`); 
+// console.log(`Before conversion type ${typeof isLoggedIn}`);
+// let checkBoolean =Boolean(isLoggedIn)
+// console.log(`After conversion ${checkBoolean}`); 
+// console.log(`After conversion type ${typeof checkBoolean}`); 
+
+// let isLoggedIn='Chinmay'
+// console.log(`Before conversion ${isLoggedIn}`); 
+// console.log(`Before conversion type ${typeof isLoggedIn}`);
+// let checkBoolean =Boolean(isLoggedIn)
+// console.log(`After conversion ${checkBoolean}`); 
+// console.log(`After conversion type ${typeof checkBoolean}`); 
+
+/*when a non zero value will convert into boolean then it will become true in boolean 
+ when a zero value/empty string  will convert into boolean then it will become false in boolean */
+
+// number to string
+
+let num=122
+
+let numString=String(num)
+console.log(numString);
+console.log(typeof numString);
